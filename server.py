1d62738e691a4b3b18e4a748deaafd25aa44b876
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2024-02-19 12:16:57 -0700

    made some changes to better display received data

--- server.py
+++ server.py
@@ -1,17 +1,21 @@
 import socket
 
-HOST = '10.200.195.152'  # Listen on this interface
-PORT = 12345       # Arbitrary port number
+HOST = "10.200.195.123"  # Listen on this interface
+PORT = 12345  # Arbitrary port number
+iterations = 0
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
     conn, addr = s.accept()
     with conn:
-        print('Connected by', addr)
+        print("Connected by", addr)
         while True:
             data = conn.recv(1024)
             if not data:
                 break
-            print('Received:', data.decode())
-
+            tempdatalist = data.decode().split("---")
+            print(f"iteration: {iterations}")
+            iterations += 1
+            for eachtempdata in tempdatalist:
+                print(eachtempdata)
